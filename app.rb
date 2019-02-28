85104da9360fc0cadf781f7fa5455dd0454f3153
Olletrolle <38528665+Olletrolle@users.noreply.github.com>
2019-02-28 10:58:34 +0100

    Login
    försökte fixa innloggningen till personlig profil

--- app.rb
+++ app.rb
@@ -17,25 +17,29 @@ get('/create') do
   slim(:create)
 end
 
-# post('/login') do
+get('/profile') do
+  slim(:create)
+end
 
-#   db = SQLite3::Database.new('./public/inlogg.db')
-#   db.results_as_hash = true
-  
-#   list = db.execute("SELECT password FROM users WHERE nickname = '#{params["nickname"]}'")
-  
-#   password = list[0][0]
-  
-#   hash_password = BCrypt::Password.create(password)
+post('/login') do
 
-#   if BCrypt::Password.new(hash_password) == params["password"]
-#       session[:id] = db.execute("SELECT id FROM users WHERE nickname = '#{params["nickname"]}'").first["id"]
-#       session[:username] = db.execute("SELECT nickname FROM users WHERE nickname = '#{params["nickname"]}'").first["nickname"]
-#       redirect('/myside/#{id}')
-#   else
-#       redirect('/')
-#   end
-# end
+  db = SQLite3::Database.new('./db/databas.db')
+  db.results_as_hash = true
+  
+  list = db.execute("SELECT password FROM users WHERE nickname = '#{params["nickname"]}'")
+  
+  password = list[0][0]
+  
+  hash_password = BCrypt::Password.create(password)
+
+  if BCrypt::Password.new(hash_password) == params["password"]
+    session[:id] = db.execute("SELECT id FROM users WHERE nickname = '#{params["nickname"]}'").first["id"]
+    session[:username] = db.execute("SELECT nickname FROM users WHERE nickname = '#{params["nickname"]}'").first["nickname"]
+    redirect("/profile")
+  else
+    redirect('/')
+  end
+end
 
 post('/create') do
   db = SQLite3::Database.new('./db/databas.db')
