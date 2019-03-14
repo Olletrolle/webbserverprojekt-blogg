abe8e023886dc6d6c6167a7999828f2a49060603
Olletrolle <38528665+Olletrolle@users.noreply.github.com>
2019-03-14 14:01:47 +0100

    Jobbade vidare
    jobbade på inläggs funktionen

--- app.rb
+++ app.rb
@@ -2,7 +2,6 @@ require 'SQlite3'
 require 'sinatra'
 require 'slim'
 require 'bcrypt'
-
 enable :sessions
 
 get('/') do
@@ -46,13 +45,14 @@ get("/profile/:id") do
   db.results_as_hash = true
 
   if session[:profile] == true
-    session[:users] = db.execute("SELECT nickname, realName FROM users WHERE id = #{session[:id]}")
+    session[:users] = db.execute("SELECT realName FROM users WHERE id = #{session[:id]}")
     slim(:profile, locals: {
       user: session[:users]
     })
   else
     redirect("/nix")
   end
+
 end
 
 post('/create') do
@@ -65,10 +65,24 @@ post('/create') do
   redircedt("/")
 end
 
-post('/edit') do
+get("/uploadPost/:id") do
+  slim(:upload)
+end
+
+post("/uploadPost/:id") do
   db = SQLite3::Database.new('./db/databas.db')
   db.results_as_hash = true
 
-  db.execute("UPDATE users SET nickname ='?', realName = '?', WHERE id = :id}", params["nickname"], params["realName"])
-  redirect("/profile/:id")
+  post_id = session[:id]
+
+  db.execute("INSERT INTO posts(title, text, post_id) VALUES(?, ?, ?)", params["title"], params["text"], post_id)
+  redircedt("/profile/:id")
 end
+
+# post('/edit') do
+#   db = SQLite3::Database.new('./db/databas.db')
+#   db.results_as_hash = true
+
+#   db.execute("UPDATE users SET nickname ='?', realName = '?', WHERE id = :id}", params["nickname"], params["realName"])
+#   redirect("/profile/:id")
+# end
